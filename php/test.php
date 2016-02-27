5ac112b29f1e3ef6dee10b0a1618c1660d24b95e
Che Shian Hung <hungc@bgsu.edu>
2016-02-27 04:21:50 -0500

    123

--- php/test.php
+++ php/test.php
@@ -7,7 +7,14 @@ Welcome <?php echo $_POST["username"]; ?><br>
 Email address: <?php echo $_POST["password"]; ?><br>
 
 <?php
- Server: carpl123.database.windows.net,1433 \r\nSQL Database: myDB\r\nUser Name: hungc\r\n\r\nPHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   $conn = new PDO ( \"sqlsrv:server = tcp:carpl123.database.windows.net,1433; Database = myDB\", \"hungc\", \"{your_password_here}\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"hungc@carpl123\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"myDB\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);\r\n$serverName = \"tcp:carpl123.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
+ Server: carpl123.database.windows.net,1433 \r\nSQL Database: myDB\r\nUser Name: hungc\r\n\r\nPHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   $conn = new PDO ( \"sqlsrv:server = tcp:carpl123.database.windows.net,1433; 
+ 	Database = myDB\", \"hungc\", \"Elac0212\");\r\n   
+ 	 $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   
+ 	 	print( \"Error connecting to SQL Server.\" );\r\n   
+ 	 	die(print_r($e));\r\n}\r\n\r
+ 	 	SQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"hungc@carpl123\", \"pwd\" => \"Elac0212\", \"Database\" => \"myDB\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);\r\n
+ 	 	$serverName = \"tcp:carpl123.database.windows.net,1433\";\r\n
+ 	 	$conn = sqlsrv_connect($serverName, $connectionInfo);
 if(!$conn)
 {
 	echo "Fail";
