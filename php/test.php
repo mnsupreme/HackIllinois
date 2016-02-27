f0c7e815fcfba29bc4ec825bb7372038f2dfe434
Che Shian Hung <hungc@bgsu.edu>
2016-02-27 04:42:33 -0500

    123

--- php/test.php
+++ php/test.php
@@ -7,7 +7,7 @@ Welcome <?php echo $_POST["username"]; ?><br>
 Email address: <?php echo $_POST["password"]; ?><br>
 
 <?php
-$conn = mysqli_connect(tcp:carpl123.database.windows.net,hungc,Elac0212)
+$conn = mysqli_connect("tcp:carpl123.database.windows.net","hungc","Elac0212");
 if(!$conn)
 {
 	die("Fail" .mysqli_connect_error());
