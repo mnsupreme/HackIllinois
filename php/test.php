ff6e5f64d9554e73160b9ddfb00fc1e2fefc9c49
Che Shian Hung <hungc@bgsu.edu>
2016-02-27 07:01:06 -0500

    123

--- php/test.php
+++ php/test.php
@@ -7,7 +7,17 @@ Welcome <?php echo $_POST["username"]; ?><br>
 Email address: <?php echo $_POST["password"]; ?><br>
 
 <?php
-Server: carpl123.database.windows.net,1433 \r\nSQL Database: myDB\r\nUser Name: hungc\r\n\r\nPHP Data Objects(PDO) Sample Code:\r\n\r\ntry {\r\n   $conn = new PDO ( \"sqlsrv:server = tcp:carpl123.database.windows.net,1433; Database = myDB\", \"hungc\", \"{your_password_here}\");\r\n    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );\r\n}\r\ncatch ( PDOException $e ) {\r\n   print( \"Error connecting to SQL Server.\" );\r\n   die(print_r($e));\r\n}\r\n\rSQL Server Extension Sample Code:\r\n\r\n$connectionInfo = array(\"UID\" => \"hungc@carpl123\", \"pwd\" => \"{your_password_here}\", \"Database\" => \"myDB\", \"LoginTimeout\" => 30, \"Encrypt\" => 1, \"TrustServerCertificate\" => 0);\r\n$serverName = \"tcp:carpl123.database.windows.net,1433\";\r\n$conn = sqlsrv_connect($serverName, $connectionInfo);
+$servername = "13.75.43.109,13.75.44.92,23.97.74.106,168.63.141.7";
+$username = "hungc@bgsu.edu";
+$password = ".Elac0212";
+
+$conn=new mysqil($servername,$username,$password);
+
+if($conn -> connect_error)
+{
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 
 $sql = "CREATE TABLE Users(name VARCHAR(30) NOT NULL,password VARCHAR(30) NOT NULL,PRIMARY KEY(name))";
 
